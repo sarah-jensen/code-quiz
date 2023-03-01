5b861b5ddf3be273b5e000ddd8ef0301a4180494
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-02-28 22:32:42 -0800

    added draft of timer element in JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,34 @@
+// button variables
+const startButton = document.querySelector('.start-button');
+const highScores = document.querySelector('.high-scores');
+const gameOver = document.querySelector('.game-over');
+
+
+// timer
+const timerEl = document.querySelector('#timer');
+const timer;
+const secondsLeft = 60;
+
+function startTimer() {
+    timer = setInterval(function() {      
+
+        if(secondsLeft >= 0) {
+            secondsLeft--;
+            timerEl.textContent = secondsLeft;
+        }else {
+            clearInterval(timer);
+            gameOver();
+        }
+    }, 1000);
+}
+
+function gameOver() {
+    timerEl.textContent = "00"
+    gameOver.setAttribute("display", "block");
+}
+
+startButton.addEventListener("click", startTimer)
+// add timer
 // array of questions
 // question = object with answers
 // "get question" implied loop driven by user click on buttons
