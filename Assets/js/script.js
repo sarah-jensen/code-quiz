012a774647e90969c33ba98eee1ffa277c1a5c89
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-03-07 18:56:12 -0800

    updated nextQuestion function to display within HTML section

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,7 +6,12 @@ const gameOver = document.querySelector(".game-over");
 const questionCard = document.querySelector(".questions");
 const submitButton = document.querySelector("#submit-button");
 const retakeButton = document.querySelector("#retake-quiz");
-let currentQuestion = 0;
+let currentQuestion = [0];
+let questionText = document.querySelector(".question-text");
+let answerA = document.querySelector("#answer-a");
+let answerB = document.querySelector("#answer-b");
+let answerC = document.querySelector("#answer-c");
+let answerD = document.querySelector("#answer-d");
 let userName = document.querySelector("#player-name");
 let scoreCounter = 0;
 
@@ -19,33 +24,53 @@ let questions = [
   {
     question:
       "You can chain together actions/methods in which programming language?",
-    answers: ["JavaScript", "Bootstrap", "CSS", "jQuery"],
-    correct: "jQuery",
+    answers: [
+      "A. JavaScript", 
+      "B. Bootstrap", 
+      "C. CSS", 
+      "D. jQuery"
+    ],
+    correct: "D. jQuery",
   },
   {
     question: "Which language is needed for a user to interact with a webpage?",
-    answers: ["CSS", "JavaScript", "HTML", "GitHub"],
-    correct: "JavaScript",
+    answers: [
+      "A. CSS", 
+      "B. JavaScript", 
+      "C. HTML", 
+      "D. GitHub"
+    ],
+    correct: "B. JavaScript",
   },
   {
     question: "In JavaScript, a group of related data is called a/an...",
-    answers: ["string", "object", "array", "function"],
-    correct: "array",
+    answers: [
+      "A. string", 
+      "B. object", 
+      "C. array", 
+      "D. function"
+    ],
+    correct: "C. array",
   },
   {
     question: "Media queries are feature of which language?",
-    answers: ["JavaScript", "HTML", "CSS", "jQuery"],
-    correct: "CSS",
+    answers: [
+      "A. JavaScript", 
+      "B. HTML", 
+      "C. CSS", 
+      "D. jQuery"
+    ],
+    correct: "C. CSS",
   },
   {
     question: "To use a function in JavaSript, you must...",
     answers: [
-      "define the function",
-      "call the function",
-      "give a condition for the function",
-      "a & b only",
+      "A. define the function",
+      "B. call the function",
+      "C. give a condition for the function",
+      "D. a & b only",
     ],
-    correct: "a & b only",
+    correct: "D. a & b only",
   },
 ];
 
@@ -61,7 +86,7 @@ function setTime() {
       clearInterval(timer);
       timerEl.innerHTML = 00;
       endGame();
-    };
+    }
   }, 1000);
 
   // displays the next question
@@ -69,6 +94,11 @@ function setTime() {
     //makes the current question !== 0
     if (currentQuestion < questions.length) {
       console.log(questions[currentQuestion]);
+      questionText.textContent = questions[currentQuestion].question;
+      answerA.textContent = questions[currentQuestion].answers[0];
+      answerB.textContent = questions[currentQuestion].answers[1];
+      answerC.textContent = questions[currentQuestion].answers[2];
+      answerD.textContent = questions[currentQuestion].answers[3];
     } else {
       console.log("Game Over");
       endGame();
@@ -76,28 +106,28 @@ function setTime() {
 
     // creates template literal (HTML outline) for the question cards
     // move this to populate HTML instead
-    let HTML = `
-    <div>
-    <h2>
-    ${questions[currentQuestion].question}
-    </h2>
-    <button class = "choices">
-    ${questions[currentQuestion].answers[0]}
-    </button>
-    <button class = "choices">
-    ${questions[currentQuestion].answers[1]}
-    </button>
-    <button class = "choices">
-    ${questions[currentQuestion].answers[2]}
-    </button>
-    <button class = "choices">
-    ${questions[currentQuestion].answers[3]}
-    </button>
-    </div>
-    `;
+    // let HTML = `
+    // <div>
+    // <h2>
+    // ${questions[currentQuestion].question}
+    // </h2>
+    // <button class = "choices">
+    // ${questions[currentQuestion].answers[0]}
+    // </button>
+    // <button class = "choices">
+    // ${questions[currentQuestion].answers[1]}
+    // </button>
+    // <button class = "choices">
+    // ${questions[currentQuestion].answers[2]}
+    // </button>
+    // <button class = "choices">
+    // ${questions[currentQuestion].answers[3]}
+    // </button>
+    // </div>
+    // `;
 
     // displays the template literal in the appropriate HTML section
-    questionCard.innerHTML = HTML;
+    // questionCard.innerHTML = HTML;
 
     let choices = document.querySelectorAll(".choices");
 
@@ -108,18 +138,16 @@ function setTime() {
 
     // set reaction for correct and incorrect answers
     function checkAnswer(event) {
-      console.log(event.target.innerText);
-      if (event.target.innerText === questions[currentQuestion].correct) {
+      console.log(event.target.textContent);
+      if (event.target.textContent === questions[currentQuestion].correct) {
         alert("Correct!");
         currentQuestion++;
         scoreCounter++;
         nextQuestion();
-      } else if (
-        event.target.innerText !== questions[currentQuestion].correct
-      ) {
+      } else if (event.target.textContent !== questions[currentQuestion].correct) {
         alert("Incorrect!");
         currentQuestion++;
-        secondsLeft = secondsLeft-5;
+        secondsLeft = secondsLeft - 5;
         nextQuestion();
       }
     }
